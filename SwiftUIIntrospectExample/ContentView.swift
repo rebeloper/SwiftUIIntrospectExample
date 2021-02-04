ff1923ef969557db46be4371bf7d1385cb599b59
Alex Nagy <therebeloper@gmail.com>
2021-02-04 11:57:08 +0200

    Initial Commit

---
+++ SwiftUIIntrospectExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIIntrospectExample
+//
+//  Created by Alex Nagy on 04.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIIntrospectExample/SwiftUIIntrospectExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntrospectExampleApp.swift
+//  SwiftUIIntrospectExample
+//
+//  Created by Alex Nagy on 04.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntrospectExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
